a90c4abea71fcf8bdbf8b3478c44a50e64a3deb0
ndidi19 <66070586+ndidi19@users.noreply.github.com>
2022-01-26 14:46:56 +0100

    Updated structure

--- src/containers/Books/AddBook/AddBook.js
+++ src/containers/Books/AddBook/AddBook.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {withFormik} from "formik";
+import * as Yup from "yup";
 
 class AddBook extends Component {
 
@@ -37,35 +38,60 @@ class AddBook extends Component {
                         value={this.props.values.name}
                         //onChange={(event) => this.setState({book: {name: event.target.value}})}
                         onChange={this.props.handleChange}
+                        onBlur={this.props.handleBlur}
                          />
+                         {
+                            this.props.errors.name && this.props.touched.name &&
+                            <span style={{color: "red"}}>{this.props.errors.name}</span>
+                         }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="author" className="form-label">Auteur :</label>
                         <input type="text" id="author" name="author" className="form-control"
                         value={this.props.values.author}
                         onChange={this.props.handleChange}
+                        onBlur={this.props.handleBlur}
                         />
+                        {
+                            this.props.errors.author && this.props.touched.author &&
+                            <span style={{color: "red"}}>{this.props.errors.author}</span>
+                         }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="price" className="form-label">Prix :</label>
                         <input type="number" id="price" name="price" className="form-control"
                         value={this.props.values.price}
-                        onChange={this.props.handleChange}                           
+                        onChange={this.props.handleChange}
+                        onBlur={this.props.handleBlur}                           
                         />
+                        {
+                            this.props.errors.price && this.props.touched.price &&
+                            <span style={{color: "red"}}>{this.props.errors.price}</span>
+                         }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="year" className="form-label">Année :</label>
                         <input type="number" id="year" name="year" className="form-control"
                         value={this.props.values.year}
-                        onChange={this.props.handleChange}                            
+                        onChange={this.props.handleChange}
+                        onBlur={this.props.handleBlur}                            
                         />
+                        {
+                            this.props.errors.year && this.props.touched.year &&
+                            <span style={{color: "red"}}>{this.props.errors.year}</span>
+                         }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="country" className="form-label">Pays :</label>
                         <input type="text" id="country" name="country" className="form-control"
                         value={this.props.values.country}
-                        onChange={this.props.handleChange}                           
+                        onChange={this.props.handleChange}   
+                        onBlur={this.props.handleBlur}                        
                         />
+                        {
+                            this.props.errors.country && this.props.touched.country &&
+                            <span style={{color: "red"}}>{this.props.errors.country}</span>
+                         }
                     </div>
                     
                     <button type="submit" className="btn btn-primary" onClick={this.props.handleSubmit}>Créer</button>
@@ -83,19 +109,21 @@ export default withFormik({
             year: "",
             country: ""
     }),
-    validate: values => {
-        const errors = {};
-        if(values.name.length < 5) {
-            errors.name = "Le titre doit avoir au moins 5 caractères";
-        }
-        if(values.name.length > 15) {
-            errors.name = "Le titre doit avoir au maximum 15 caractères";
-        }
-        if(!values.author) {
-            errors.author = "Le champ auteur est requis";
-        }
-        return errors;
-    },
+    validationSchema : Yup.object().shape({
+        name: Yup.string()
+                    .min(3, "Le nom du livre doit comporter au minimum 3 caractères...")
+                    .max(15, "Le nom du livre doit comporter au maximum 15 caractères...")
+                    .required(),
+        author: Yup.string()
+                    .required("Le nom de l'auteur est requis"),
+        price: Yup.number()
+                    .required("Le prix est requis"),
+        year: Yup.number()
+                    .moreThan(1900, "L'année doit être supérieur 1900")
+                    .required("L'année est requise"),
+        country: Yup.string()
+                    .required("Le nom du pays est requis"),
+    }),
     handleSubmit: (values, {props}) => {
         console.log(values);
         props.callToAction(values);
