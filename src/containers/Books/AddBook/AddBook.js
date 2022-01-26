9dd7c74e166516eeee7b5e7e9e711bb69ac2f0c2
ndidi19 <66070586+ndidi19@users.noreply.github.com>
2022-01-26 13:43:23 +0100

    update

--- src/containers/Books/AddBook/AddBook.js
+++ src/containers/Books/AddBook/AddBook.js
@@ -1,20 +1,30 @@
 import React, {Component} from "react";
+import {withFormik} from "formik";
 
 class AddBook extends Component {
 
-    state = {
-        name: "",
-        author: "",
-        price: "",
-        year: "",
-        country: ""
-    }
+    // state = {
+    //     book: {
+    //         name: "",
+    //         author: "",
+    //         price: "",
+    //         year: "",
+    //         country: ""
+    //     }
+    // }
 
-    validationFormHandler = (event) => {
-        event.preventDefault();
-        console.log("Soumission du form");
-        this.props.callToAction(this.state.name, this.state.author, this.state.price, this.state.year, this.state.country)
-    }
+    // validationFormHandler = (event) => {
+    //     event.preventDefault();
+    //     console.log("Soumission du form");
+    //     this.props.callToAction(this.state.book);
+    //     this.setState({
+    //         name: "",
+    //         author: "",
+    //         price: "",
+    //         year: "",
+    //         country: ""
+    //     });
+    // }
 
     render() {
         return (
@@ -24,44 +34,70 @@ class AddBook extends Component {
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Nom :</label>
                         <input type="text" id="name" name="name" className="form-control"
-                        value={this.state.name}
-                        onChange={(event) => this.setState({name: event.target.value})}
+                        value={this.props.values.name}
+                        //onChange={(event) => this.setState({book: {name: event.target.value}})}
+                        onChange={this.props.handleChange}
                          />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="author" className="form-label">Auteur :</label>
                         <input type="text" id="author" name="author" className="form-control"
-                        value={this.state.author}
-                        onChange={(event) => this.setState({author: event.target.value})}
-                         />
+                        value={this.props.values.author}
+                        onChange={this.props.handleChange}
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="price" className="form-label">Prix :</label>
                         <input type="number" id="price" name="price" className="form-control"
-                        value={this.state.price}
-                        onChange={(event) => this.setState({price: event.target.value})}
-                         />
+                        value={this.props.values.price}
+                        onChange={this.props.handleChange}                           
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="year" className="form-label">Année :</label>
                         <input type="number" id="year" name="year" className="form-control"
-                        value={this.state.year}
-                        onChange={(event) => this.setState({year: event.target.value})}
-                         />
+                        value={this.props.values.year}
+                        onChange={this.props.handleChange}                            
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="country" className="form-label">Pays :</label>
                         <input type="text" id="country" name="country" className="form-control"
-                        value={this.state.country}
-                        onChange={(event) => this.setState({country: event.target.value})}
-                         />
+                        value={this.props.values.country}
+                        onChange={this.props.handleChange}                           
+                        />
                     </div>
                     
-                    <button type="submit" className="btn btn-primary" onClick={this.validationFormHandler}>Créer</button>
-                    </form>
+                    <button type="submit" className="btn btn-primary" onClick={this.props.handleSubmit}>Créer</button>
+                </form>
             </>
         )
     }
 }
 
-export default AddBook;
+export default withFormik({
+    mapPropsToValues : () => ({
+            name: "",
+            author: "",
+            price: "",
+            year: "",
+            country: ""
+    }),
+    validate: values => {
+        const errors = {};
+        if(values.name.length < 5) {
+            errors.name = "Le titre doit avoir au moins 5 caractères";
+        }
+        if(values.name.length > 15) {
+            errors.name = "Le titre doit avoir au maximum 15 caractères";
+        }
+        if(!values.author) {
+            errors.author = "Le champ auteur est requis";
+        }
+        return errors;
+    },
+    handleSubmit: (values, {props}) => {
+        console.log(values);
+        props.callToAction(values);
+    }
+}) (AddBook);
